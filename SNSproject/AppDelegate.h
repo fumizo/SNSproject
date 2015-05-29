9de8b556641e4dbe78a4c035458fcb46f4745220
fumiko <fumi811bbc@gmail.com>
2015-05-29 19:36:09 +0900

    Initial Commit

---
+++ SNSproject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SNSproject
+//
+//  Created by 山本文子 on 2015/05/29.
+//  Copyright (c) 2015年 山本文子. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SNSproject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SNSproject
+//
+//  Created by 山本文子 on 2015/05/29.
+//  Copyright (c) 2015年 山本文子. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
