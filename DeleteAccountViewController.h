d8552bbab853f2e781fba7d729866024fbee65fc
fumiko <fumi811bbc@gmail.com>
2015-06-27 19:22:06 +0900

    push通知を実装

---
+++ DeleteAccountViewController.h
@@ -0,0 +1,13 @@
+//
+//  DeleteAccountViewController.h
+//  SNSproject
+//
+//  Created by 山本文子 on 2015/06/13.
+//  Copyright (c) 2015年 山本文子. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DeleteAccountViewController : UIViewController
+
+@end
---
+++ DeleteAccountViewController.m
@@ -0,0 +1,37 @@
+//
+//  DeleteAccountViewController.m
+//  SNSproject
+//
+//  Created by 山本文子 on 2015/06/13.
+//  Copyright (c) 2015年 山本文子. All rights reserved.
+//
+
+#import "DeleteAccountViewController.h"
+
+@interface DeleteAccountViewController ()
+
+@end
+
+@implementation DeleteAccountViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
